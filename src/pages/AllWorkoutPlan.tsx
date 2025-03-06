7c9dda40330c7c69bbc6c2a60377d81441e0ed36
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2025-03-06 10:19:47 +0530

    changes

--- src/pages/AllWorkoutPlan.tsx
+++ src/pages/AllWorkoutPlan.tsx
@@ -18,8 +18,16 @@ const AllWorkoutPlan = () => {
     limit: 5,
     offset: currentPage * 5,
   });
-  
-  const isLastPage = !data || data.length === 1;
+
+  if (isLoading) return <WorkoutDayLoader />;
+  if (!data)
+    return (
+      <div className="bg-MainBackgroundColor w-full min-h-screen p-4">
+        <h1 className="text-SecondaryTextColor text-center mt-10">No Plans</h1>
+      </div>
+    );
+
+  const isLastPage = data.length === 1;
   const isFirstPage = currentPage === 0;
 
   const handleNextBtn = () => {
@@ -34,28 +42,35 @@ const AllWorkoutPlan = () => {
     }
   };
 
-  if (isLoading) return <WorkoutDayLoader />;
-
   return (
     <div className="bg-MainBackgroundColor w-full min-h-screen p-4 font-poppins relative">
       <h1 className="text-PrimaryTextColor text-xl font-semibold">All Plans</h1>
-      <div className="min-h-screen mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data?.map((item) => (
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {data.map((item) => (
           <WorkoutPlanCard planDetails={item} key={item.id} />
         ))}
       </div>
-      <Pagination className="mt-4">
+
+      {data.length === 0 && (
+        <h1 className="text-SecondaryTextColor text-center mt-10">No Plans Go Back</h1>
+      )}
+
+      <Pagination className="mt-4 absolute left-0 right-0 bottom-0">
         <PaginationContent className="w-full flex justify-between">
           <PaginationItem>
             <PaginationPrevious
               onClick={isFirstPage ? undefined : handlePreviousBtn}
-              className={`text-PrimaryTextColor hover:bg-transparent hover:text-PrimaryTextColor ${currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""}`}
+              className={`text-PrimaryTextColor hover:bg-transparent hover:text-PrimaryTextColor ${
+                currentPage === 0 ? "opacity-50 cursor-not-allowed" : ""
+              }`}
             />
           </PaginationItem>
           <PaginationItem>
             <PaginationNext
               onClick={isLastPage ? undefined : handleNextBtn}
-              className={`text-PrimaryTextColor hover:bg-transparent hover:text-PrimaryTextColor ${!data || data.length < 5 ? "opacity-50 cursor-not-allowed" : ""}`}
+              className={`text-PrimaryTextColor hover:bg-transparent hover:text-PrimaryTextColor ${
+                data.length < 5 ? "opacity-50 cursor-not-allowed" : ""
+              }`}
             />
           </PaginationItem>
         </PaginationContent>
