4d333430d86b9e688f014f41c16f37126ac6f0a1
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2025-03-07 00:49:23 +0530

    update loader

--- src/components/loaders/Loader.tsx
+++ src/components/loaders/Loader.tsx
@@ -1,9 +1,9 @@
-import { ClipLoader } from "react-spinners";
+import { BarLoader} from "react-spinners";
 
 const Loader = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-MainBackgroundColor">
-      <ClipLoader color="#fff" size={60} />
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-MainBackgroundColor">
+      <BarLoader color="#a6ff17" />
     </div>
   );
 };
