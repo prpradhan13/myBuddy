612122e532f04ec2b1e3c66f0c8bd6e148be91b6
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2025-03-11 01:44:16 +0530

    make some changes in vdo UI

--- src/pages/ExerciseVisuals.tsx
+++ src/pages/ExerciseVisuals.tsx
@@ -24,7 +24,7 @@ const ExerciseVisuals = () => {
   }
 
   return (
-    <div className="w-full h-screen relative">
+    <div className="w-full h-screen relative scrollbar-hidden-y">
       <div className="fixed inset-0 -z-10">
         <AdvancedVideo
           cldVid={myVideo}
@@ -36,24 +36,20 @@ const ExerciseVisuals = () => {
       </div>
 
       {/* Scrollable Text Content */}
-      <div className="absolute bottom-0 left-0 w-full max-h-[40vh] bg-gradient-to-t from-black to-transparent overflow-y-auto p-6 text-white scrollbar-hidden-y">
-        <h1 className="text-4xl font-bold">{exerciseWithSets?.exercise_name}</h1>
-        <h2 className="text-xl font-semibold mt-1">{exerciseWithSets?.target_muscle}</h2>
-        <p className="mt-4">{exerciseWithSets?.exercise_description}</p>
-        <p className="my-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-          malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Vestibulum auctor dapibus neque, id auctor leo.
-        </p>
-
-        {exerciseWithSets?.exercise_sets?.map((item, index) => (
-            <div key={item.id} className="mt-4">
-                <h3 className="text-lg font-medium">Set {index + 1}</h3>
-                <p>Target Repetitions - {item.target_repetitions}</p>
-                <p>Target Weight - {item.target_weight}</p>
-            </div>
-        ))}
+      <div className="absolute top-0 left-0 overflow-y-auto text-white">
+        <div className="bg-transparent h-[60vh]"></div>
+        <div className="w-full bg-gradient-to-t from-black to-transparent p-6">
+          <h1 className="text-4xl font-bold">{exerciseWithSets?.exercise_name}</h1>
+          <h2 className="text-lg font-semibold mt-1">{exerciseWithSets?.target_muscle}</h2>
+          <p className="mt-4">{exerciseWithSets?.exercise_description}</p>
+          {exerciseWithSets?.exercise_sets?.map((item, index) => (
+              <div key={item.id} className="my-4">
+                  <h3 className="text-lg font-medium">Set {index + 1}</h3>
+                  <p>Target Repetitions - {item.target_repetitions}</p>
+                  <p>Target Weight - {item.target_weight}</p>
+              </div>
+          ))}
+        </div>
       </div>
     </div>
   );
